140493a9ac10cc1e94c57a320d7e7ac28be41f63
shawn gatchell <shawng03@gmail.com>
2017-05-03 15:05:15 -0700

    create funnel by grouping by week in sql

--- app/controllers/funnels_controller.rb
+++ app/controllers/funnels_controller.rb
@@ -1,8 +1,8 @@
 class FunnelsController < ApplicationController
   def index
     start_date = params[:start_date] || Date.today - 1.month
-    end_date   = params[:end_date]   || Date.today
-    @funnel = get_funnel start_date, end_date
+    end_date   = params[:end_date]   || Date.tomorrow
+    @funnel = make_funnel(start_date, end_date)
 
     respond_to do |format|
       format.html { @chart_funnel = formatted_funnel }
@@ -30,10 +30,24 @@ class FunnelsController < ApplicationController
     end
   end
 
-  def get_funnel(start_date, end_date)
-    h = {}
-    h['2014-12-01-2014-12-07'] = {'applied' => 5, 'hired' => 1}
-    h['2014-11-01-2014-11-07'] = {'applied' => 1, 'rejected' => 2}
-    h
+  def make_funnel(start_date, end_date)
+    funnel = {}
+    # assumes Applicant.created_at can represent the date applicant applied 
+    by_weeks = Applicant.where('created_at >= ? AND created_at <= ?', start_date, end_date)
+      .select('created_at, workflow_state, COUNT(workflow_state) AS workflow_state_count, strftime("%Y%W", created_at) as week')
+      .group('week, workflow_state')
+    by_weeks.group_by(&:week).values.each do |counts|
+
+      date_in_week = counts.sample.created_at
+      monday = date_in_week - (date_in_week.strftime("%w").to_i - 1).days
+      sunday = monday + 6.days
+      week_range = "#{monday.strftime '%F'}-#{sunday.strftime '%F'}"
+      
+      counts_hash = {}
+      counts.each {|c| counts_hash[c.workflow_state] = c.workflow_state_count}
+      funnel[week_range] = counts_hash
+    end
+
+    funnel
   end
 end

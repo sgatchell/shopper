14a395b081acb4d735844419881ce13cbae976b9
shawn gatchell <shawng03@gmail.com>
2017-05-03 15:57:12 -0700

    basic form

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,10 +1,17 @@
 class ApplicantsController < ApplicationController
   def new
-    # your code here
+    @applicant = Applicant.new
+    
   end
 
   def create
-    # your code here
+    @applicant = Applicant.new(applicant_params.merge(workflow_state: 'applied'))
+    if @applicant.save
+      render text: 'good work!'
+    else
+      ap @applicant.errors.full_messages
+      render :new
+    end
   end
 
   def update
@@ -13,5 +20,9 @@ class ApplicantsController < ApplicationController
 
   def show
     # your code here
+  end
+
+  def applicant_params
+    params.require(:applicant).permit(:email, :first_name, :last_name, :phone, :phone_type, :region)
   end
 end
